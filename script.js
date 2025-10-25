d5c1d48e8bdfba41d8e0960401c15c10eac7b110
William Liu <will.liu1705@gmail.com>
2025-10-25 19:21:13 -0400

    Add a simple interactive UI for the game
    - the player now has three buttons to select for their move
    - each round will announce the winner
    - after someone gets five points, the winner is announced and the game resets on next button click

--- script.js
+++ script.js
@@ -1,71 +1,101 @@
 "use strict";
 
 function getComputerChoice() {
-  let value = Math.floor(Math.random() * 3)
+  let value = Math.floor(Math.random() * 3);
 
   if (value === 0) {
-    return "rock"
+    return "rock";
   } else if (value === 1) {
-    return "paper"
+    return "paper";
   }
   else {
-    return "scissors"
+    return "scissors";
   }
 }
 
-function getHumanChoice() {
-  let userChoice = prompt("rock, paper, or scissors?")
-
-  return userChoice;
+function announceWinnerOfGame(gameWinner) {
+  let announceWinnerDiv = document.createElement("div");
+  announceWinnerDiv.classList.add("game-winner");
+  switch (gameWinner) {
+    case "Computer":
+      announceWinnerDiv.textContent = `${gameWinner} has won the game!`;
+      break;
+    case "You":
+      announceWinnerDiv.textContent = `${gameWinner} have won the game!`;
+      break;
+  }
+  gameDiv.appendChild(announceWinnerDiv);
 }
 
-function playGame() {
-  let roundNumber = 1;
-  let computerScore = 0;
-  let humanScore = 0;
+function announceRoundWinner(roundWinner) {
+  let roundWinnerDiv = document.createElement("div");
+  roundWinnerDiv.classList.add("round-winner");
+  switch (roundWinner) {
+    case "Computer":
+      roundWinnerDiv.textContent = `${roundWinner} has won the round.`;
+      break;
+    case "You":
+      roundWinnerDiv.textContent = `${roundWinner} have won the round.`;
+      break;
+    case "draw":
+      roundWinnerDiv.textContent = `It's a ${roundWinner}.`;
+      break
+  }
+  gameDiv.appendChild(roundWinnerDiv);
+}
 
-  let computerChoice = getComputerChoice();
-  let humanChoice = getHumanChoice();
+function clearCounters() {
+  computerScoreSpan.textContent = "0";
+  humanScoreSpan.textContent = "0";
+}
 
-  function playRound(computerChoice, humanChoice) {
-    humanChoice = humanChoice.toLowerCase();
+function clearWinnerAnnouncements() {
+  if (document.querySelector(".game-winner")) {
+    // we also need to remove the round winner div as well
+    document.querySelector(".round-winner").remove();
+    document.querySelector(".game-winner").remove();
+    clearCounters();
+  } else if (document.querySelector(".round-winner")) {
+    document.querySelector(".round-winner").remove();
+  }
+}
 
-    // rock beats scissors
-    // scissors beats paper
-    // paper beats rock
-    if (computerChoice === "rock" && humanChoice === "scissors"
-      || computerChoice === "scissors" && humanChoice === "paper"
-      || computerChoice === "paper" && humanChoice === "rock") {
-
-      computerScore++;
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
-    } else if (humanChoice === "rock" && computerChoice === "scissors"
-      || humanChoice === "scissors" && computerChoice === "paper"
-      || humanChoice === "paper" && computerChoice === "rock") {
-      console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
-      humanScore++;
-    } else {
-      console.log("It's a draw!");
-    }
-
-    if (roundNumber != 5) {
-      roundNumber += 1;
-      computerChoice = getComputerChoice();
-      humanChoice = getHumanChoice();
-      playRound(computerChoice, humanChoice);
-    } else {
-      if (computerScore > humanScore) {
-        console.log("Computer wins the game!")
-      } else if (computerScore < humanScore) {
-        console.log("Congratulations, you win!")
-      } else {
-        console.log("The game is a tie.")
-      }
-      return;
-    }
+function playRound(computerChoice, humanChoice) {
+  clearWinnerAnnouncements();
+  // rock beats scissors
+  // scissors beats paper
+  // paper beats rock
+  if (computerChoice === "rock" && humanChoice === "scissors"
+    || computerChoice === "scissors" && humanChoice === "paper"
+    || computerChoice === "paper" && humanChoice === "rock") {
+    computerScoreSpan.textContent++;
+    announceRoundWinner("Computer");
+  } else if (humanChoice === "rock" && computerChoice === "scissors"
+    || humanChoice === "scissors" && computerChoice === "paper"
+    || humanChoice === "paper" && computerChoice === "rock") {
+    humanScoreSpan.textContent++;
+    announceRoundWinner("You");
+  } else {
+    announceRoundWinner("draw");
   }
 
-  playRound(computerChoice, humanChoice);
+  if (Number(computerScoreSpan.textContent) === 5) {
+    announceWinnerOfGame("Computer");
+  } else if (Number(humanScoreSpan.textContent) === 5) {
+    announceWinnerOfGame("You");
+  }
 }
 
-playGame();
+let allChoices = document.querySelectorAll("button");
+let gameDiv = document.querySelector(".game");
+let computerScoreSpan = document.querySelector(".computer-score");
+let humanScoreSpan = document.querySelector(".human-score");
+
+allChoices.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    let humanChoice = event.target.textContent.toLowerCase();
+    let computerChoice = getComputerChoice();
+    playRound(computerChoice, humanChoice);
+  })
+});
+
